1602c96802ce08496f03358e45ca28bedc86956b
sanketghodke <109572200+sanketghodke@users.noreply.github.com>
2022-07-21 14:32:20 +0530

    Deleted_Initial_django

--- Todo/home/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import *
-from django.contrib.auth.models import User
-
-class NotesForm(forms.ModelForm):
-    class Meta:
-        model=Notes
-        exlude=['date']
-        fields=['author','note_title','note_detail','is_done']
